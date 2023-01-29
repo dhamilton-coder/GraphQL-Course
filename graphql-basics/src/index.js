83d15cb9614b5d90223755b9c473ce06976b8098
David Hamilton <davidpatrick1000r@gmail.com>
2023-01-29 18:12:20 +0000

    First Query

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,15 @@
-console.log('Test')
+import { GraphQLServer } from 'graphql-yoga'
+
+const typeDefs = `
+    type Query {
+    hello: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query'
+        }
+    }
+}
