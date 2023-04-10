4b943365102a207d0f3c1ed7574d411475ac8c57
David Hamilton <davidpatrick1000r@gmail.com>
2023-04-10 22:12:26 +0100

    Returned Arrays of Custom Types and allowed basic query funcionality

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,11 +2,59 @@ import { GraphQLServer } from 'graphql-yoga'
 const chalk = require('chalk')
 
 
+
+const examplePosts = [{
+  ID: 1291081,
+  title: 'The Myth of Freedom',
+  body: 'This is not what I had hoped for when writing this blog. Endless critisism from individuals who think they have it all under their control. Their life in their hands, I wish, alas it is unfortuantly the way things are and what can someone like me do about that. Sorry guys!',
+  published : true
+  
+},
+
+{
+
+  ID : 327480928,
+  title: 'The Wonders of the Sea',
+  body: `Sorry, i'm to lazy to write another blog. `,
+  published : false
+},
+
+{
+
+  ID : 32748092843,
+  title: 'How did we get here? ( Evolution : A basic guide )',
+  body: `Sorry, i'm to lazy to write another blog. `,
+  published : false
+}]
+
+
+const exampleData = [{
+
+  ID : 21312312,
+  Nachname : 'Bond',
+  Vorname : 'Billy',
+  age : 49
+
+},
+
+{
+  ID : 2131223111,
+Nachname : 'David',
+Vorname : 'Hangerson',
+age : 67
+},
+
+{
+  ID : 8219038120,
+  Nachname : 'Hello',
+  Vorname : 'Jello',
+  age : 109
+}]
+
 const typeDefs = `
 type Query {
-  greeting(name: String) : String
-  grades(amount_correct : [Float]! , total : [Float]!) : Float
-  add(a: [Float]!) : Float 
+  users(query : String) : [User!]!
+  getPosts(body : String, title : String, ID : Int, published : Boolean): [Post!]!
   me : User!
   post : Post!
 }
@@ -48,42 +96,55 @@ const resolvers = {
       } 
     },
 
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-      return `Hello ${args.name}`
-      } 
-      else {
-        return 'Sorry. No name Provided!'
+    users (parent, args) {
+      if (args.query) {
+        return exampleData.filter((user) => {
+          return user.Nachname.toLowerCase().includes(args.query.toLowerCase())
+        })
+      } else {
+        return exampleData
       }
+      
+    
     },
 
-    add(parent, args) {
-      if (args.a.length  === 0) {
-        return 0
+    
+    getPosts (parent, args) {
+  
+       if (args.ID) {
+        return examplePosts.filter((post) => {
+          return post.ID.includes(args.ID)
+        })
+      }
+
+      else if (args.title) {
+        return examplePosts.filter((post) => {
+          return post.title.toLowerCase().includes(args.title.toLowerCase())
+        })
+      }
+
+      else if (args.body) {
+        return examplePosts.filter((post) => {
+          return post.body.toLowerCase().includes(args.body.toLowerCase())
+        })
+     
+      }
+
+      else if (args.published) {
+        return examplePosts.filter((post) => {
+          return post.published.includes(args.published)
+        })
       }
 
       else {
-       const sum = args.a.reduce((partialSum, a) => partialSum + a, 0); 
-       return sum
+        return examplePosts
       }
-    },
-
-    grades(parent, args, context, info) {
-
-      if (args.total.length === 0 || args.amount_correct.length === 0) {
-        return 0
-      }
-      else {
-        const sum = args.amount_correct.reduce((partialSum, a) => partialSum + a, 0); 
-        const sum2 = args.total.reduce((partialSum,a) => partialSum + a, 0)
-
-        return sum / sum2 * 100
-      }
-      return [10, 20, args.amount_correct / args.total * 100]
+      
+    
     }
-            
-      }
-    }
+
+  }
+}
 
 
 const server = new GraphQLServer ({
