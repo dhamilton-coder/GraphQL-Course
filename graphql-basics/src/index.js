6228de2c7b2b576837e508e095ab745bfb1d1206
David Hamilton <davidpatrick1000r@gmail.com>
2023-04-15 19:59:47 +0100

    Added Comments and set up relationships

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,12 +2,39 @@ import { GraphQLServer } from 'graphql-yoga'
 const chalk = require('chalk')
 
 
+const exampleComments = [{
+  ID: 12910812321,
+  text: 'Comment One',
+  author : '2131223111',
+  post : 1291081
+  
+},
+
+{
+
+  ID : 327480928231231,
+  text : 'Comment Two',
+  author : '8219038120',
+  post : 1291081
+},
+
+{
+
+  ID : 32748092843213123,
+  text: 'Comment Three',
+  author : '2131223111',
+  post : 1291081
+
+}]
+
+
 
 const examplePosts = [{
   ID: 1291081,
   title: 'The Myth of Freedom',
   body: 'This is not what I had hoped for when writing this blog. Endless critisism from individuals who think they have it all under their control. Their life in their hands, I wish, alas it is unfortuantly the way things are and what can someone like me do about that. Sorry guys!',
-  published : true
+  published : true,
+  author : '2131223111'
   
 },
 
@@ -16,7 +43,8 @@ const examplePosts = [{
   ID : 327480928,
   title: 'The Wonders of the Sea',
   body: `Sorry, i'm to lazy to write another blog. `,
-  published : false
+  published : false,
+  author : '21312312'
 },
 
 {
@@ -24,13 +52,14 @@ const examplePosts = [{
   ID : 32748092843,
   title: 'How did we get here? ( Evolution : A basic guide )',
   body: `Sorry, i'm to lazy to write another blog. `,
-  published : false
+  published : false,
+  author : '2131223111'
 }]
 
 
 const exampleData = [{
 
-  ID : 21312312,
+  ID : '21312312',
   Nachname : 'Bond',
   Vorname : 'Billy',
   age : 49
@@ -38,14 +67,14 @@ const exampleData = [{
 },
 
 {
-  ID : 2131223111,
+  ID : '2131223111',
 Nachname : 'David',
 Vorname : 'Hangerson',
 age : 67
 },
 
 {
-  ID : 8219038120,
+  ID : '8219038120',
   Nachname : 'Hello',
   Vorname : 'Jello',
   age : 109
@@ -57,6 +86,7 @@ type Query {
   getPosts(body : String, title : String, ID : Int, published : Boolean): [Post!]!
   me : User!
   post : Post!
+  getComments : [Comment!]
 }
 
 
@@ -64,7 +94,10 @@ type User {
   ID : ID!
   Nachname : String!
   Vorname : String!
-  age: Int!
+  age: String!
+  posts : [Post!]!
+  comments : [Comment!]!
+
 },
 
 type Post {
@@ -72,7 +105,15 @@ type Post {
   title : String!
   body :  String!
   published : Boolean!
+  author : User!,
+}
 
+
+type Comment {
+  ID: ID!
+  text: String!
+  author : User!
+  post : Post!
 }
 `
 
@@ -80,7 +121,7 @@ const resolvers = {
     Query: {
     me() {
       return {
-        ID : 21312312,
+        ID : '21312312',
         Nachname : 'Bond',
         Vorname : 'Billy',
         age : 49
@@ -92,7 +133,7 @@ const resolvers = {
         ID: 1291081,
         title: 'The Myth of Freedom',
         body: 'This is not what I had hoped for when writing this blog. Endless critisism from individuals who think they have it all under their control. Their life in their hands, I wish, alas it is unfortuantly the way things are and what can someone like me do about that. Sorry guys!',
-        published : true
+        published : true,
       } 
     },
 
@@ -141,10 +182,57 @@ const resolvers = {
       }
       
     
+    },
+
+    getComments (parent, args) {
+      return exampleComments
     }
 
+  },
+  Post: {
+    author(parent, args) {
+      return exampleData.find((user) => {
+        return user.ID === parent.author
+      })
+    }
+  },
+
+  User: {
+
+    posts(parent, args) {
+      return examplePosts.filter((post) => {
+        return post.author === parent.ID
+          
+      })
+    },
+    comments(parent,args) {
+    return exampleComments.filter((comment) => {
+      return comment.author === parent.ID
+    })
   }
+
+
+  },
+
+  Comment : {
+    author(parent, args) {
+     return exampleData.find((user) => {
+      return user.ID === parent.author
+     })
+    },
+
+
+    post(parent, args) {
+      return examplePosts.find((post) => {
+       return post.ID === parent.post
+      })
+     }
+  
+  }
+  
 }
+  
+
 
 
 const server = new GraphQLServer ({
